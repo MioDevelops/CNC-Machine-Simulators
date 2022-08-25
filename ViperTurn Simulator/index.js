3423e67397d4c6dc1fa22008a95c8196b83f380b
Mio Yanaka <94136291+MioDevelops@users.noreply.github.com>
2022-08-25 15:58:57 +0000

    modified

--- ViperTurn Simulator/index.js
+++ ViperTurn Simulator/index.js
@@ -1,4 +1,16 @@
+var lastKey = null;
+var count = 0;
 function input(e, type="default") {
-    console.log(e.innerHTML.split(/[><]+/)[2], "Type: " + type)
-    //this is where it will add to the monitor/screen
+    if(lastKey != e.innerHTML.split(/[><]+/)[2]){
+        lastKey = e.innerHTML.split(/[><]+/)[2];
+        var Repeat = false;
+    } else {
+        Repeat = true;
+        count++;
+        if(count == 2){
+            Repeat = false;
+            count = 0;
+        }
+    }
+    console.log("| Key:", e.innerHTML.split(/[><]+/)[2], "Type:", type, "Repeat: ", Repeat, '|');
 };
